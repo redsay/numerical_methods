a637a7d38ad12f43d3ae9c06ca3795fbf796dfb9
David-Zagaceta <davidzagaceta@gmail.com>
2024-04-14 15:03:01 -0700

    automatically installing package requirements from setup.py script now

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import os
+
 from setuptools import setup
+
+
+def _read_requirements():
+    with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+        return f.read().splitlines()
+
 
 setup(
     name="ode_solve",
     packages=["ode_solve"],
     version="0.1",
+    install_requires=_read_requirements(),
     description="""A package implementing analytic, semi-analytic,
     numerical and machine learning methods for solving ordinary differential equations""",
 )
