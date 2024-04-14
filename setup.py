e76d56e1d0aac790cbe330f7be2f616825dc5f76
David-Zagaceta <davidzagaceta@gmail.com>
2024-04-13 23:22:30 -0700

    set up repo boilerplate

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ode_solve",
+    packages=["ode_solve"],
+    version="0.1",
+    description="""A package implementing analytic, semi-analytic,
+    numerical and machine learning methods for solving ordinary differential equations""",
+)
